fb0d45824520544ae1f26eb71629d00b04845d45
Vlad0n1m <VladKarachkov321@gmail.com>
2023-04-15 09:41:09 +0600

    basic components for profile page

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src='https://www.ixbt.com/img/n1/news/2019/5/3/chrome-73-mode-sombre-android_large.jpg' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+
+      <nav className="nav">
+        <div><a href="#">Home</a></div>
+        <div><a href="#">Feed</a></div>
+        <div><a href="#">Messages</a></div>
+        <div><a href="#">News</a></div>
+        <div><a href="#">Music</a></div>
+        <div><a href="#">Settings</a></div>
+      </nav>
+  )
+}
+
+
+export default Navbar;
